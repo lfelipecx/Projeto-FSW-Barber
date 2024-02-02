75a18eed5d21d5cc559f4bffd28cfcab3c74c813
Luiz Felipe <lfelipecx@gmail.com>
2024-02-02 16:49:59 -0300

    feat: add toast on booking success

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -13,6 +13,8 @@ import { generateDayTimeList } from "../_helpers/hours";
 import { format, setHours, setMinutes } from "date-fns";
 import { saveBooking } from "../_actions/save-booking";
 import { Loader2 } from "lucide-react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 
 
@@ -23,11 +25,13 @@ interface ServiceItemProps {
 }
 
 const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps) => {
+    const router = useRouter()
     const {data} = useSession()
 
     const [date, setDate] = useState<Date | undefined>(undefined)
     const [hour, setHour] = useState<String | undefined>()
     const [submitIsLoading, setSubmitIsLoading] = useState(false);
+    const [sheetIsOpen, setSheetIsOpen] = useState(false);
 
     const handleDateClick = (date: Date | undefined) => {
         setDate(date)
@@ -63,6 +67,19 @@ const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps)
                 date: newDate,
                 userId: (data.user as any).id
             })
+
+            setSheetIsOpen(false)
+            setHour(undefined);
+            setDate(undefined);
+            toast("Reserva realizada com sucesso!", {
+                description: format(newDate, "'Para' dd 'de' MMMM 'às' HH':'mm'.'", {
+                  locale: ptBR,
+                }),
+                action: {
+                  label: "Visualizar",
+                  onClick: () => router.push("/bookings"),
+                },
+            });
         }catch(error){
             console.error(error)
         } finally {
@@ -100,7 +117,7 @@ const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps)
                                 }).format(Number(service.price))}
                             </p>
 
-                            <Sheet>
+                            <Sheet open={sheetIsOpen} onOpenChange={setSheetIsOpen}>
                                 <SheetTrigger asChild>
                                     <Button variant="secondary" onClick={handleBookingClick}>
                                         Reservar
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./_components/footer";
 import AuthProvider from "./_providers/auth";
+import { Toaster } from "./_components/ui/sonner"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,8 @@ export default function RootLayout({
         <AuthProvider>
           
           {children}
+
+          <Toaster />
           <Footer />
 
         </AuthProvider>
