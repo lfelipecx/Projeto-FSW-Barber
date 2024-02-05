105a32f0caadbaff889eb898c85f07ddc9e2048e
Luiz Felipe <lfelipecx@gmail.com>
2024-02-04 21:17:54 -0300

    feat: add cursor pointer BookingItem component

--- app/_actions/cancel-booking.ts
+++ app/_actions/cancel-booking.ts
@@ -1,15 +1,14 @@
 "use server"
 
-import { revalidatePath } from "next/cache"
 import { db } from "../_lib/prisma"
+import { revalidatePath } from "next/cache"
 
 export const cancelBooking = async (bookingId: string) => {
-    return await db.booking.delete({
+    await db.booking.delete({
         where: {
             id: bookingId
         }
-    });
-    
+    })
 
     revalidatePath("/")
     revalidatePath("/bookings")
